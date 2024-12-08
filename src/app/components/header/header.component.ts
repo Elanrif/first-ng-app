204bc772a451f5529fa460f2bd47c1b28d49f319
Elanrif <saidbacoelanrif@gmail.com>
2024-12-08 23:33:43 +0100

    feat: It means we just have to use our service in header.component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  providers: [TodoService]
 })
 export class HeaderComponent {
    title = 'My Angular Application'
