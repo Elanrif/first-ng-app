99a56a0b462ffea4e97bc2beb20ca25e430b6f95
Elanrif <91225994+Elanrif@users.noreply.github.com>
2024-12-09 13:44:37 +0100

    ng g c [directorie/component] --dry-run (#1)
    * feat: ng g c [directorie/component] --dry-run
    
    * feat: we call a component within a template with here selector
    
    * feat: call a component with selector name
    
    * Data biding another way without signal
    
    * Using signal to data binding
    
    * Data biding from parent component
    
    * like in react, but quite difference. we just pass
    a string
    
    * with [message] we have to pass a javascript code
    
    * feat: change value with set, but this is old ways
    
    * feat: now we change the value like in react state with update functionality
    
    * feat: angular routes
    
    * feat: It means we just have to use our service in header.component
    
    * feat: providedIn Root  means we can use our providers where we want
    
    * feat: inject todo service
    
    * feat: onInit it is a function called after component initiation
    
    * feat: ngOnInit it is a function called after component initiation
    
    * feat: using inject signal and ngOnInit() together
    
    * feat: HttpClient providers
    
    * feat: use directive for more additional actions
    
    * feat: use directive without ng, another way
    
    * feat: another way to use directive called controll flow block without *ngName
    
    * ...
    
    * feat: directives
    
    * feat: Pipe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: async() => {
+        return import ('./home/home.component').then((m) => m.HomeComponent)
+    },
+    },
+    {
+     path: 'todos',
+     loadComponent: async() => {
+        return import('./todos/todos.component').then((m) => m.TodosComponent)
+     }
+    }
+];
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss',
+})
+export class CounterComponent {
+  countervalue = signal(0);
+  increment() {
+    this.countervalue.update((val) => val + 1);
+  }
+
+  decrement() {
+    this.countervalue.update((val) => val - 1);
+  }
+
+  reset() {
+    this.countervalue.set(0);
+  }
+}
